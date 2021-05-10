cec9bbe3e2e27876b00f2c5f6652a41c149c41e5
fares <larouchifares.07@gmail.com>
2021-05-10 08:48:33 +0100

    first react

---
+++ src/App.js
@@ -0,0 +1,88 @@
+import './App.css';
+import {Form, Button, Col,Nav,Navbar,NavDropdown, FormControl,Alert} from 'react-bootstrap'
+
+function App() {
+  
+  return (
+    <>
+  
+  <Navbar  expand="lg" className='nav'>
+  <Navbar.Brand href="#home">Login </Navbar.Brand>
+  <Navbar.Toggle aria-controls="basic-navbar-nav" />
+  <Navbar.Collapse id="basic-navbar-nav">
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#link">About</Nav.Link>
+      <NavDropdown title="Location" id="basic-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Action 1</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Action 2</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Action 3</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Others</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Form inline>
+      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+      <Button variant="outline-success" className="search">Search</Button>
+    </Form>
+  </Navbar.Collapse>
+</Navbar>
+<main className='main'>
+     <Form>
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridEmail">
+      <Form.Label>Email</Form.Label>
+      <Form.Control type="email" placeholder="Enter email" />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridPassword">
+      <Form.Label>Password</Form.Label>
+      <Form.Control type="password" placeholder="Password" />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group controlId="formGridAddress1">
+    <Form.Label>Address</Form.Label>
+    <Form.Control placeholder="1234 Main St" />
+  </Form.Group>
+
+  <Form.Group controlId="formGridAddress2">
+    <Form.Label>Address 2</Form.Label>
+    <Form.Control placeholder="Apartment, studio, or floor" />
+  </Form.Group>
+
+  <Form.Row>
+    <Form.Group as={Col} controlId="formGridCity">
+      <Form.Label>City</Form.Label>
+      <Form.Control />
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridState">
+      <Form.Label>State</Form.Label>
+      <Form.Control as="select" defaultValue="Choose...">
+        <option>Choose...</option>
+        <option>...</option>
+      </Form.Control>
+    </Form.Group>
+
+    <Form.Group as={Col} controlId="formGridZip">
+      <Form.Label>Zip</Form.Label>
+      <Form.Control />
+    </Form.Group>
+  </Form.Row>
+
+  <Form.Group id="formGridCheckbox">
+    <Form.Check type="checkbox" label="Check me out" />
+  </Form.Group>
+
+  <Button variant="primary" type="submit">
+    Submit
+  </Button>
+</Form>
+
+</main>
+    </>
+  );
+}
+
+export default App;
